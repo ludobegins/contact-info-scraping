5b95cb497e0d6de9eef2dd1cbbcfad1522984258
ludobegins <ludo.beghin@gmail.com>
2022-04-19 19:23:02 -0300

    linkedin contacts a cada 10 min 1 empresa

--- linkedin_contacts.py
+++ linkedin_contacts.py
@@ -1,10 +1,14 @@
 # source: https://practicaldatascience.co.uk/data-science/how-to-scrape-google-search-results-using-python
+# CUIDADO COM BLOCK: https://stackoverflow.com/questions/22657548/is-it-ok-to-scrape-data-from-google-results#:~:text=Also%20the%20block%20will%20not,not%20if%20you%20keep%20going.
+# Melhor não rodar mais de 8 search requests por hora
 
+from encodings import utf_8
 import requests
 import urllib
 import pandas as pd
 import empresas_aux
 from requests_html import HTMLSession
+from time import sleep
 
 def get_source(url):
     """Return the source code for the provided URL. 
@@ -27,7 +31,6 @@ def get_source(url):
 def get_results(query):
     
     query = urllib.parse.quote_plus(query)
-    print(query)
     response = get_source("https://www.google.com.br/search?q=" + query)
     
     return response
@@ -40,7 +43,6 @@ def parse_results(response):
     #css_identifier_text = ".VwiC3b"
     
     results = response.html.find(css_identifier_result)
-    print(results)
 
     output = []
     
@@ -62,14 +64,19 @@ def google_search(query):
 
 def save_search(termo,path1):
     results = google_search(termo)
-    path = path1+f'{termo}.csv'
+    save = termo.replace(".com","").replace(":"," ")
+    path = path1+f'{save}_v2.csv'
     df = pd.DataFrame(results)
-    df.to_csv(path, mode='w', header=True)
+    print(df.head(10))
+    df.to_csv(path, mode='w', header=True,encoding='utf-8-sig')
 
 path1 = r'C:\gitrepos\contact-info-scraping\contatos\''
-termo_complem = " sustentabilidade linkedin"
-for e in empresas_aux.lista_empresas[:1]:
+termo_complem = " sustentabilidade site:linkedin.com"
+for e in empresas_aux.lista_empresas[21:31]:
     save_search(e+termo_complem,path1)
+    sleep(600)
+
+#save_search("BCO BRASIL"+termo_complem,path1)
 
 #termo_pesquisa = input("Termo de pesquisa:\n")
 
